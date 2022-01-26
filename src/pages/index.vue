f3876f9411baca327098d892cdcea54c034e1bf7
Joaquin leimeter <leimeter.joaquin@gmail.com>
2022-01-26 16:54:32 -0300

    got list going on index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,62 +1,41 @@
 <script setup lang="ts">
-import { useUserStore } from '~/stores/user'
-
-const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-const go = () => {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-
-const { t } = useI18n()
+const todoList = ref([
+  {
+    id: 0,
+    text: 'go to bathroom',
+    completed: true,
+  },
+  {
+    id: 1,
+    text: 'flush',
+    completed: false,
+  },
+])
 </script>
 
 <template>
-  <div>
-    <p class="text-4xl">
-      <carbon-campsite class="inline-block" />
-    </p>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em class="text-sm opacity-75">{{ t('intro.desc') }}</em>
-    </p>
-
-    <div class="py-4" />
-
-    <input
-      id="input"
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      :aria-label="t('intro.whats-your-name')"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
+  <div class="container">
+    <div class="todo-list">
+      <ul>
+        <li v-for="item in todoList" :key="item.id">
+          {{ item.text }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .container{
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+  }
+  .todo-list{
+    background-color: burlywood;
+    padding: 50px;
+  }
+</style>
 
 <route lang="yaml">
 meta:
