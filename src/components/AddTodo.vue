df9fe537e7fb3f5cea702223c82c3765d2cf4a69
JoaquinLeimeter <joaquin.leimeter@radiumrocket.com>
2022-01-28 17:10:34 -0300

    form styles

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-    const inputText = ref('')
+const inputText = ref('')
 
-    const emit = defineEmits(["taskSubmitted"]);
+const emit = defineEmits(["taskSubmitted"]);
 
-    const onSubmit = () => {
+const onSubmit = () => {
   inputText.value && emit('taskSubmitted', inputText.value)
-    }
+}
 
 const inputTextError = computed(() => inputText.value?.length >= 50);
 
@@ -15,16 +15,41 @@ watch(inputTextError, (value, oldValue) => {
 </script>
 
 <template>
-    <div class="container">
-        <h3>{{ inputText }}</h3>
-        <form @submit.prevent="onSubmit">
+  <div class="formContainer">
+    <form @submit.prevent="onSubmit">
       <label for="text">Add Task:</label>
-            <input id="text" placeholder="wash clothes" v-model="inputText">
+      <input id="text" placeholder="wash clothes" v-model="inputText">
       <span :class="inputTextError && 'spanError'">{{ inputText.length }} of 50 characters</span>
-            <button type="submit" value="Submit">Add!</button>
-        </form>
-    </div>
+      <button type="submit" value="Submit">Add!</button>
+    </form>
+  </div>
 </template>
 
 <style>
+.formContainer {
+  background-color: #008080;
+  padding: 1rem;
+  margin-top: 25px;
+}
+.formContainer > form{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.formContainer > form > div > label{
+  padding: 0.6rem;
+}
+.formContainer button {
+  border: 2px solid #FFF;
+  border-radius: 3px;
+  padding: 0.5rem 1.5rem;
+  margin: 0.3rem;
+}
+.formContainer input {
+  margin: 0.5rem;
+  padding: 0.5rem;
+}
+.spanError {
+  color: red;
+}
 </style>
