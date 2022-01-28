3c04483bf78d598f483e2b678a8e74733007ecda
JoaquinLeimeter <joaquin.leimeter@radiumrocket.com>
2022-01-28 17:09:30 -0300

    added watch and computed

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -6,14 +6,21 @@
     const onSubmit = () => {
         emit('taskSubmitted', inputText.value) // value from inputs is always a string
     }
+
+const inputTextError = computed(() => inputText.value?.length >= 50);
+
+watch(inputTextError, (value, oldValue) => {
+  inputTextError.value && alert('warning, do not exeed 50 characters');
+});
 </script>
 
 <template>
     <div class="container">
         <h3>{{ inputText }}</h3>
         <form @submit.prevent="onSubmit">
-            <label for="text">Task:</label>
+      <label for="text">Add Task:</label>
             <input id="text" placeholder="wash clothes" v-model="inputText">
+      <span :class="inputTextError && 'spanError'">{{ inputText.length }} of 50 characters</span>
             <button type="submit" value="Submit">Add!</button>
         </form>
     </div>
