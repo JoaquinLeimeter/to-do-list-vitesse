73c98e9e3c7c42b7d89663d0fdfd99f14055212b
JoaquinLeimeter <joaquin.leimeter@radiumrocket.com>
2022-01-29 15:45:15 -0300

    changed styles and added closeModal emit

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 const inputText = ref('')
 
-const emit = defineEmits(["taskSubmitted"]);
+const emit = defineEmits(["taskSubmitted", "closeModal"]);
 
 const onSubmit = () => {
-  inputText.value && !inputTextError.value && emit('taskSubmitted', inputText.value)
+  inputText.value &&
+    !inputTextError.value &&
+    emit('taskSubmitted', inputText.value) &&
+    emit('closeModal')
 }
 
 const inputTextError = computed(() => inputText.value?.length >= 50);
@@ -17,8 +20,7 @@ watch(inputTextError, () => {
 <template>
   <div class="formContainer">
     <form @submit.prevent="onSubmit">
-      <label for="text">Add Task:</label>
-      <input id="text" placeholder="wash clothes" v-model="inputText">
+      <input id="text" placeholder="wash clothes" v-model="inputText" />
       <span :class="inputTextError && 'spanError'">{{ inputText.length }} of 50 characters</span>
       <button type="submit" value="Submit">Add!</button>
     </form>
@@ -29,18 +31,17 @@ watch(inputTextError, () => {
 .formContainer {
   background-color: #008080;
   padding: 1rem;
-  margin-top: 25px;
 }
-.formContainer > form{
+.formContainer > form {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
-.formContainer > form > div > label{
+.formContainer > form > div > label {
   padding: 0.6rem;
 }
 .formContainer button {
-  border: 2px solid #FFF;
+  border: 2px solid #fff;
   border-radius: 3px;
   padding: 0.5rem 1.5rem;
   margin: 0.3rem;
