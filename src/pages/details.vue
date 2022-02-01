aed84bb0304e80626d74b3c61015700c4c5c87cc
JoaquinLeimeter <joaquin.leimeter@radiumrocket.com>
2022-02-01 12:42:27 -0300

    step 6: create route that shows component

---
+++ src/pages/details.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ToDoDetails from '~/components/ToDoDetails.vue';
+</script>
+
+<template>
+  <ToDoDetails></ToDoDetails>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
